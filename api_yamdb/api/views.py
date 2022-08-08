3ae6833d8450dad5415bed198c27acd5fb1929e6
alex-s-nik <to_ctrl@mail.ru>
2022-08-09 00:16:52 +0300

    add review and comment models

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+class CommentViewSet(viewsets.ModelViewSet):
+    ...
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    ...
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Review(models.Model):
+    text = models.TextField(verbose_name='Текст')
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор'
+    )
+    score = models.SmallIntegerField(
+        verbose_name='Оценка',
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Время добавления',
+        auto_now_add=True
+    )
+
+
+class Comment(models.Model):
+    text = models.TextField(verbose_name='Текст')
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор'
+    )
+    review = models.ForeignKey(
+        to=Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Ревью'
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Время добавления',
+        auto_now_add=True
+    )
