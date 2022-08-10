5387511a78a02bf61f769b4f25c2bed6991e3a17
alex-s-nik <to_ctrl@mail.ru>
2022-08-10 15:40:08 +0300

    изменены вьюсеты Comments и review

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,22 +1,32 @@
 from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from reviews.models import Comment, Review
+from .serializers import ReviewSerializer, CommentSerializer
+
 
 class CommentViewSet(viewsets.ModelViewSet):
-    # pagination_class = ...
+    pagination_class = LimitOffsetPagination
     # permission_classes = ...
-    # serializer_class = ...
+    serializer_class = CommentSerializer
+
     def perform_create(self, serializer):
         return super().perform_create(serializer)
 
     def get_queryset(self):
-        return super().get_queryset()
+        review_id = self.kwargs.get('review_id')
+        comments_queryset = Comment.objects.filter(review=review_id)
+        return comments_queryset
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
-    # pagination_class = ...
+    pagination_class = LimitOffsetPagination
     # permission_classes = ...
-    # serializer_class = ...
+    serializer_class = ReviewSerializer
+
     def perform_create(self, serializer):
         return super().perform_create(serializer)
 
     def get_queryset(self):
-        return super().get_queryset()
+        title_id = self.kwargs.get('title_id')
+        review_queryset = Review.objects.filter(title=title_id)
+        return review_queryset
