daffe75f3a6658bbfce648dd1da25b22c1a251da
alex-s-nik <to_ctrl@mail.ru>
2022-08-15 17:57:11 +0300

    добавлены докстринги

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
+    """Пользователь проекта YaMDB"""
     ROLE_CHOICES = (
         ('user', 'Пользователь'),
         ('moderator', 'Модератор'),
